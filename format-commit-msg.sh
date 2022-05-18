1e80f434fe3e704d600ca1708e1e905b130c2282
KBS9622 <58119450+KBS9622@users.noreply.github.com>
2022-05-18 17:43:43 +0800

    Update format-commit-msg.sh

--- format-commit-msg.sh
+++ format-commit-msg.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # Automatically adds branch name and branch description to every commit message.
 # Modified from the gist here https://gist.github.com/bartoszmajsak/1396344
