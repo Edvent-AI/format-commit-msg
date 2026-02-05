ab8fe790408558a92f8ea245f57ef0b32dbd45f1
Tiger Yotsawat <tigeryotsawat@gmail.com>
2026-02-05 00:03:10 +0000

    add proper quoting for file path variable

--- format-commit-msg.sh
+++ format-commit-msg.sh
@@ -39,10 +39,10 @@ BRANCH_PREFIX_REGEX="^\[[a-zA-Z0-9]+-[a-zA-Z0-9]+\]"
 BRANCH_EXCLUDED=$(printf "%s\n" "${BRANCHES_TO_SKIP[@]}" | grep -c "^$BRANCH_NAME$")
 
 # Whether the commit message has a TICKET_ID
-BRANCH_IN_COMMIT=$(grep -c "$TICKET_ID" $1)
+BRANCH_IN_COMMIT=$(grep -c "$TICKET_ID" "$1")
 
 # Get the first line of the commit message
-COMMIT_MSG=$(head -n 1 $1)
+COMMIT_MSG=$(head -n 1 "$1")
 
 # Check if branch is excluded (like main)
 if [[ $BRANCH_EXCLUDED -eq 1 ]]; then
@@ -69,7 +69,7 @@ if [[ $BRANCH_EXCLUDED -eq 1 ]]; then
 elif [[ "$BRANCH_NAME" =~ $VALID_BRANCH_REGEX ]]; then
   # Valid feature branch - auto-prepend if needed
   if [ -n "$BRANCH_NAME" ] && ! [[ $BRANCH_IN_COMMIT -ge 1 ]]; then
-    sed -i -e "1s:^:[$TICKET_ID] :" $1
+    sed -i -e "1s:^:[$TICKET_ID] :" "$1"
   fi
 else
   # Invalid branch name
